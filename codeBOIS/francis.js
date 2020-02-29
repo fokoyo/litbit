cf46bb9175991a7b93c0a3dee9e2a8e0554b286e
Francis Okoyo <francis@cengtech.com>
2020-02-29 13:54:30 -0500

    added step counter

--- codeBOIS/francis.js
+++ codeBOIS/francis.js
@@ -1,18 +1,63 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+import { Pedometer } from 'expo-sensors';
+
+const styles = StyleSheet.create({
+    root: {
+        textAlign: 'center',
+        // backgroundColor: 'blue',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    count: {
+        fontWeight: 'bold',
+        fontSize: 150
+    },
+    count_text: {
+        fontSize: 50
+    }
+});
 
 
-const Work = ({
-    ...props
+const CountDisplay = ({
+    count = 0,
+    ...other
 }) => {
-
+    console.log('count display:', count);
     return (
-        <View>
-            <Text>
-                Francis
+        <View style={styles.root}>
+            <Text style={styles.count}>
+                {count}
+            </Text>
+            <Text style={styles.count_text}>
+                steps
             </Text>
         </View>
     );
 }
 
+const Work = ({
+    ...props
+}) => {
+
+    const [count, setCount] = useState(0);
+
+
+    useEffect(() => {
+        Pedometer.watchStepCount(result => {
+            setCount(result.steps);
+        });
+    }, []);
+
+    return (
+        <View>
+            <CountDisplay count={count} />
+        </View>
+    )
+
+
+}
 export default Work;
