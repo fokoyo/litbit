68f0c06ff815265d61bc885b78354635dc707e01
Francis Okoyo <francis@cengtech.com>
2020-02-29 11:47:19 -0500

    new code form your boi francis

---
+++ codeBOIS/francis.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+const Work = ({
+    ...props
+}) => {
+
+    return (
+        <View>
+            <Text>
+                Francis
+            </Text>
+        </View>
+    );
+}
+
+export default Work;
---
+++ codeBOIS/index.js
@@ -0,0 +1,1 @@
+export {default as Francis} from './francis';
